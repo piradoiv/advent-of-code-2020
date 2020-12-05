9d69d58cb5054638c56814cdae1f27b33f9249a4
Ricardo Cruz <piradoiv@gmail.com>
2020-12-05 12:11:00 +0000

    More formatting

--- src/day4.jl
+++ src/day4.jl
@@ -37,7 +37,9 @@ validPassport(passport, validator) =
 puzzleInput = open(f -> read(f, String), "input/day4.txt")
 
 # Part One
-partOneValidator(field) = field[1] in ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"]
+partOneValidator(field) =
+    field[1] in ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"]
+
 validPassports(puzzleInput, partOneValidator)
 
 # Part Two
@@ -46,11 +48,18 @@ partTwoValidator(field) =
         "byr" => 1920 <= parse(Int64, field[2]) <= 2002
         "iyr" => 2010 <= parse(Int64, field[2]) <= 2020
         "eyr" => 2020 <= parse(Int64, field[2]) <= 2030
-        "hgt", if occursin("cm", field[2]) end => replace(field[2], "cm" => "") |> x -> parse(Int64, x) |> x -> 150 <= x <= 193
-        "hgt", if occursin("in", field[2]) end => replace(field[2], "in" => "") |> x -> parse(Int64, x) |> x -> 59 <= x <= 76
+        "hgt", if occursin("cm", field[2]) end =>
+            replace(field[2], "cm" => "") |>
+            x -> parse(Int64, x) |>
+            x -> 150 <= x <= 193
+        "hgt", if occursin("in", field[2]) end =>
+            replace(field[2], "in" => "") |>
+            x -> parse(Int64, x) |>
+            x -> 59 <= x <= 76
         "hcl" => match(r"^#[0-9|a-f|A-F]{6,6}$", field[2]) != nothing
         "ecl" => match(r"^(amb|blu|brn|gry|grn|hzl|oth)$", field[2]) != nothing
         "pid" => match(r"^[0-9]{9,9}$", field[2]) != nothing
         _ => false
     end
+
 validPassports(puzzleInput, partTwoValidator)
