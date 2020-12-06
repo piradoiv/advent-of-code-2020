f0424b34cf1a556a9de7a11e26f0c2e95cfe2b5b
Ricardo Cruz <piradoiv@gmail.com>
2020-12-06 16:07:08 +0000

    Refactor Day 6

--- src/day6.jl
+++ src/day6.jl
@@ -1,22 +1,15 @@
 puzzleInput =
     open(f -> read(f, String), "input/day6.txt") |>
     strip |>
-    x -> split(x, "\n\n") |>
-    x -> String.(x)
+    x -> split(x, "\n\n") |> x -> String.(x)
+
+sumLengths(arr) = length.(arr) |> sum
 
 partOne(input) =
-    replace.(input, '\n' => "") |>
-    x -> split.(x, "") |>
-    x -> unique.(x) |>
-    x -> length.(x) |>
-    sum
-
-println("Part One: ", partOne(puzzleInput))
+    replace.(input, "\n" => "") |> x -> split.(x, "") |> x -> unique.(x) |> sumLengths
 
 partTwo(input) =
-    split.(input, "\n") |>
-    x -> map(y -> intersect(y...), x) |>
-    x -> length.(x) |>
-    sum
+    split.(input, "\n") |> x -> map(y -> intersect(y...), x) |> sumLengths
 
+println("Part One: ", partOne(puzzleInput))
 println("Part Two: ", partTwo(puzzleInput))
